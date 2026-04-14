25b29c155791cfc474ff8c048c9e2f93fcb5f2d8
Forrest Cordova <fcordova0616@gmail.com>
2026-04-14 16:16:09 -0500

    updates

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,5 +2,5 @@
 // Put your Formspree endpoint here (for contact page).
 // Example: https://formspree.io/f/xxxxxxx
 window.SITE_CONFIG = {
-  FORMSPREE_ENDPOINT: "https://formspree.io/f/maqqykgr"
+  FORMSPREE_ENDPOINT: "https://formspree.io/f/xgorvpqn"
 };
